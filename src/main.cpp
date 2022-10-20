1708d76c80429f74ed31848a287bedebc28e6d06
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2022-10-20 18:09:29 -0400

    feat: talking to root server

---
+++ src/main.cpp
@@ -0,0 +1,115 @@
+#include <Arduino.h> 
+#include <ArduinoJson.h> 
+#include <WiFiClientSecure.h>
+
+#define WIFI_SSID "***"
+#define WIFI_PASS "***"
+
+const char ISRG_ROOT_X1[] PROGMEM = R"====(
+-----BEGIN CERTIFICATE-----
+MIIFYDCCBEigAwIBAgIQQAF3ITfU6UK47naqPGQKtzANBgkqhkiG9w0BAQsFADA/
+MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT
+DkRTVCBSb290IENBIFgzMB4XDTIxMDEyMDE5MTQwM1oXDTI0MDkzMDE4MTQwM1ow
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwggIiMA0GCSqGSIb3DQEB
+AQUAA4ICDwAwggIKAoICAQCt6CRz9BQ385ueK1coHIe+3LffOJCMbjzmV6B493XC
+ov71am72AE8o295ohmxEk7axY/0UEmu/H9LqMZshftEzPLpI9d1537O4/xLxIZpL
+wYqGcWlKZmZsj348cL+tKSIG8+TA5oCu4kuPt5l+lAOf00eXfJlII1PoOK5PCm+D
+LtFJV4yAdLbaL9A4jXsDcCEbdfIwPPqPrt3aY6vrFk/CjhFLfs8L6P+1dy70sntK
+4EwSJQxwjQMpoOFTJOwT2e4ZvxCzSow/iaNhUd6shweU9GNx7C7ib1uYgeGJXDR5
+bHbvO5BieebbpJovJsXQEOEO3tkQjhb7t/eo98flAgeYjzYIlefiN5YNNnWe+w5y
+sR2bvAP5SQXYgd0FtCrWQemsAXaVCg/Y39W9Eh81LygXbNKYwagJZHduRze6zqxZ
+Xmidf3LWicUGQSk+WT7dJvUkyRGnWqNMQB9GoZm1pzpRboY7nn1ypxIFeFntPlF4
+FQsDj43QLwWyPntKHEtzBRL8xurgUBN8Q5N0s8p0544fAQjQMNRbcTa0B7rBMDBc
+SLeCO5imfWCKoqMpgsy6vYMEG6KDA0Gh1gXxG8K28Kh8hjtGqEgqiNx2mna/H2ql
+PRmP6zjzZN7IKw0KKP/32+IVQtQi0Cdd4Xn+GOdwiK1O5tmLOsbdJ1Fu/7xk9TND
+TwIDAQABo4IBRjCCAUIwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMCAQYw
+SwYIKwYBBQUHAQEEPzA9MDsGCCsGAQUFBzAChi9odHRwOi8vYXBwcy5pZGVudHJ1
+c3QuY29tL3Jvb3RzL2RzdHJvb3RjYXgzLnA3YzAfBgNVHSMEGDAWgBTEp7Gkeyxx
++tvhS5B1/8QVYIWJEDBUBgNVHSAETTBLMAgGBmeBDAECATA/BgsrBgEEAYLfEwEB
+ATAwMC4GCCsGAQUFBwIBFiJodHRwOi8vY3BzLnJvb3QteDEubGV0c2VuY3J5cHQu
+b3JnMDwGA1UdHwQ1MDMwMaAvoC2GK2h0dHA6Ly9jcmwuaWRlbnRydXN0LmNvbS9E
+U1RST09UQ0FYM0NSTC5jcmwwHQYDVR0OBBYEFHm0WeZ7tuXkAXOACIjIGlj26Ztu
+MA0GCSqGSIb3DQEBCwUAA4IBAQAKcwBslm7/DlLQrt2M51oGrS+o44+/yQoDFVDC
+5WxCu2+b9LRPwkSICHXM6webFGJueN7sJ7o5XPWioW5WlHAQU7G75K/QosMrAdSW
+9MUgNTP52GE24HGNtLi1qoJFlcDyqSMo59ahy2cI2qBDLKobkx/J3vWraV0T9VuG
+WCLKTVXkcGdtwlfFRjlBz4pYg1htmf5X6DYO8A4jqv2Il9DjXA6USbW1FzXSLr9O
+he8Y4IWS6wY7bCkjCWDcRQJMEhg76fsO3txE+FiYruq9RUWhiF1myv4Q6W+CyBFC
+Dfvp7OOGAN6dEOM4+qR9sdjoSYKEBpsr6GtPAQw4dy753ec5
+-----END CERTIFICATE-----
+)====";
+
+boolean connectedWiFi = false;
+boolean connectedRoot = false;
+
+WiFiClientSecure client;
+
+//wifi event handler
+void WiFiEvent(WiFiEvent_t event){
+    switch(event) {
+      case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+          //When connected set 
+          Serial.print("WiFi connected! IP address: ");
+          Serial.println(WiFi.localIP());  
+          connectedWiFi = true;
+          break;
+      case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+          Serial.println("WiFi lost connection");
+          connectedWiFi = false;
+          connectedRoot = false;
+          break;
+      default: 
+        break;
+    }
+}
+
+void connectToWiFi(const char * ssid, const char * pwd) {
+  Serial.println("Connecting to WiFi network: " + String(ssid));  
+  WiFi.disconnect(true);
+  WiFi.onEvent(WiFiEvent);
+  WiFi.begin(ssid, pwd);
+  Serial.println("Waiting for WiFi connection...");
+}
+
+void connectToRoot(const char * host, const int port, const char * cert) {
+  Serial.println("Connecting to Root server: " + String(host));
+  client.setCACert(cert);
+  if (!client.connect(host, port)) {
+    Serial.println("Connection failed!");
+    connectedRoot = false;
+  } else {
+    Serial.println("Connected to Root server!");
+    connectedRoot = true;
+  }
+}
+
+#define ATSIGN "soccer0"
+#define CRAM_KEY "***"
+#define ROOT_HOST "root.atsign.org"
+#define ROOT_PORT 64
+
+
+void setup() {
+   Serial.begin(115200); 
+   while (!Serial);
+   Serial.println(ROOT_HOST);
+   connectToWiFi(WIFI_SSID, WIFI_PASS);
+   connectToRoot(ROOT_HOST, ROOT_PORT, ISRG_ROOT_X1);
+} 
+
+void loop() {
+  Serial.println(ROOT_HOST);
+  if(connectedWiFi && connectedRoot) {
+    Serial.println("Printing atSign: " + String(ATSIGN) + " to Root server");
+    client.print(ATSIGN);
+    client.print("\n");
+    String address = client.readStringUntil('\r');
+    Serial.println(address);
+  } else if(connectedWiFi && !connectedRoot) {
+    connectToRoot(ROOT_HOST, ROOT_PORT, ISRG_ROOT_X1);
+  } else if(!connectedWiFi) {
+    connectToWiFi(WIFI_SSID, WIFI_PASS);
+  }
+  //Wait 1 second
+  delay(1000);
+}
